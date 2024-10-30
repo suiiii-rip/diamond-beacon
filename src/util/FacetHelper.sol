1f58f6d8e9e09d264243a7f7a452233817b26da2
suiiii <suiiii@suiiii.rip>
2024-10-30 12:04:31 +0100

    feat: FacetHelper selector to array functions

--- src/util/FacetHelper.sol
+++ src/util/FacetHelper.sol
@@ -34,6 +34,131 @@ library FacetHelper {
         return r;
     }
 
+    function asArray(bytes4 a) internal pure returns (bytes4[] memory) {
+        bytes4[] memory arr = new bytes4[](1);
+        arr[0] = a;
+        return arr;
+    }
+
+    function asArray(bytes4 a, bytes4 b) internal pure returns (bytes4[] memory) {
+        bytes4[] memory arr = new bytes4[](2);
+        arr[0] = a;
+        arr[1] = b;
+        return arr;
+    }
+
+    function asArray(bytes4 a, bytes4 b, bytes4 c) internal pure returns (bytes4[] memory) {
+        bytes4[] memory arr = new bytes4[](3);
+        arr[0] = a;
+        arr[1] = b;
+        arr[2] = c;
+        return arr;
+    }
+
+    function asArray(bytes4 a, bytes4 b, bytes4 c, bytes4 d) internal pure returns (bytes4[] memory) {
+        bytes4[] memory arr = new bytes4[](4);
+        arr[0] = a;
+        arr[1] = b;
+        arr[2] = c;
+        arr[3] = d;
+        return arr;
+    }
+
+    function asArray(bytes4 a, bytes4 b, bytes4 c, bytes4 d, bytes4 e) internal pure returns (bytes4[] memory) {
+        bytes4[] memory arr = new bytes4[](5);
+        arr[0] = a;
+        arr[1] = b;
+        arr[2] = c;
+        arr[3] = d;
+        arr[4] = e;
+        return arr;
+    }
+
+    function asArray(bytes4 a, bytes4 b, bytes4 c, bytes4 d, bytes4 e, bytes4 f)
+        internal
+        pure
+        returns (bytes4[] memory)
+    {
+        bytes4[] memory arr = new bytes4[](6);
+        arr[0] = a;
+        arr[1] = b;
+        arr[2] = c;
+        arr[3] = d;
+        arr[4] = e;
+        arr[5] = f;
+        return arr;
+    }
+
+    function asArray(bytes4 a, bytes4 b, bytes4 c, bytes4 d, bytes4 e, bytes4 f, bytes4 g)
+        internal
+        pure
+        returns (bytes4[] memory)
+    {
+        bytes4[] memory arr = new bytes4[](7);
+        arr[0] = a;
+        arr[1] = b;
+        arr[2] = c;
+        arr[3] = d;
+        arr[4] = e;
+        arr[5] = f;
+        arr[6] = g;
+        return arr;
+    }
+
+    function asArray(bytes4 a, bytes4 b, bytes4 c, bytes4 d, bytes4 e, bytes4 f, bytes4 g, bytes4 h)
+        internal
+        pure
+        returns (bytes4[] memory)
+    {
+        bytes4[] memory arr = new bytes4[](8);
+        arr[0] = a;
+        arr[1] = b;
+        arr[2] = c;
+        arr[3] = d;
+        arr[4] = e;
+        arr[5] = f;
+        arr[6] = g;
+        arr[7] = h;
+        return arr;
+    }
+
+    function asArray(bytes4 a, bytes4 b, bytes4 c, bytes4 d, bytes4 e, bytes4 f, bytes4 g, bytes4 h, bytes4 i)
+        internal
+        pure
+        returns (bytes4[] memory)
+    {
+        bytes4[] memory arr = new bytes4[](9);
+        arr[0] = a;
+        arr[1] = b;
+        arr[2] = c;
+        arr[3] = d;
+        arr[4] = e;
+        arr[5] = f;
+        arr[6] = g;
+        arr[7] = h;
+        arr[8] = i;
+        return arr;
+    }
+
+    function asArray(bytes4 a, bytes4 b, bytes4 c, bytes4 d, bytes4 e, bytes4 f, bytes4 g, bytes4 h, bytes4 i, bytes4 j)
+        internal
+        pure
+        returns (bytes4[] memory)
+    {
+        bytes4[] memory arr = new bytes4[](10);
+        arr[0] = a;
+        arr[1] = b;
+        arr[2] = c;
+        arr[3] = d;
+        arr[4] = e;
+        arr[5] = f;
+        arr[6] = g;
+        arr[7] = h;
+        arr[8] = i;
+        arr[9] = j;
+        return arr;
+    }
+
     function asAddCut(bytes4[] memory selectors, address target) internal pure returns (IDiamond.FacetCut[] memory) {
         IDiamond.FacetCut memory c = IDiamond.FacetCut(target, IDiamond.FacetCutAction.Add, selectors);
 
